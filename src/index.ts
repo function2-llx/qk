813b56e04dad496295df528a48848de983fd138c
function2 <function2-llx@outlook.com>
2021-09-15 23:20:47 +0800

    修复登录状态异常

--- src/index.ts
+++ src/index.ts
@@ -53,15 +53,19 @@ puppeteer.launch().then(async browser => {
         const page = await browser.newPage();
         await page.goto(MAIN_URL);
         if (page.url() != MAIN_URL) {
-            for (;;) {
-                await page.goto('http://zhjwxk.cic.tsinghua.edu.cn/xklogin.do');
+            async function getCaptcha() {
                 const res = await page.goto('http://zhjwxk.cic.tsinghua.edu.cn/login-jcaptcah.jpg?captchaflag=login1');
                 const buffer = await res.buffer();
                 fs.writeFileSync('captcha.jpg', buffer);
                 // const result = await fateadm.recognize(buffer.toString('base64'), '30500');
                 // const captcha = result.Result.toUpperCase();
-                const captcha: string = await new Promise(resolve => rl.question('manual captcha: ', answer => resolve(answer.toUpperCase())));
-                logger.info(`验证码：${captcha}`);
+                const captcha = await new Promise(resolve => rl.question('手动输入验证码: ', answer => resolve(answer.toUpperCase())));
+                return captcha as string;
+            };
+            for (;;) {
+                await page.goto('http://zhjwxk.cic.tsinghua.edu.cn/xklogin.do');
+                const captcha = await getCaptcha();
+                logger.info(`得到验证码：${captcha}`);
     
                 const authUrl = new URL('https://zhjwxk.cic.tsinghua.edu.cn/j_acegi_formlogin_xsxk.do');
                 authUrl.search = new URLSearchParams({
@@ -72,7 +76,7 @@ puppeteer.launch().then(async browser => {
                 }).toString();
                 await page.goto(authUrl.toString());
                 if (page.url() == MAIN_URL) {
-                    fs.writeFileSync(path.join(resultsFolder, `${captcha}.jpg`), buffer);
+                    fs.renameSync('captcha.jpg', path.join(resultsFolder, `${captcha}.jpg`));
                     break;
                 }
                 logger.info('登录失败，稍后重试');
@@ -99,33 +103,38 @@ puppeteer.launch().then(async browser => {
             //     .then(src => page.goto(src as string));
             const coursesFrame = await page.frames().find(frame => frame.name() == 'fcxkFrm')!;
 
-            console.log(conf.courses, success);
             while (success.length < conf.courses.length) {
                 for (let course of conf.courses) {
                     if (success.includes(course)) continue;
                     logger.info(`尝试提交课程：${course}`);
-                    await new Promise(async resolve => {
+                    const submitOk = await new Promise(async resolve => {
                         // 首先注册提交后对话框处理逻辑
                         page.once('dialog', async dialog => {
                             const msg = dialog.message();
                             logger.info(`${course} 提交信息：${msg}`);
+                            page.screenshot({ path: 'screenshot.png' });
                             if (msg == '提交选课成功;') {
                                 success.push(course);
-                                logger.info(`${course} 选课成功`);
+                                logger.info(`${course} 选课成功\n`);
                             } else {
-                                logger.info(`${course} 提交失败`);
+                                logger.info(`${course} 提交失败\n`);
                             }
-                            resolve(await dialog.accept());
+                            await dialog.accept();
+                            resolve(true);
                         });
 
                         // 处理提交后出现的异常情况（主要是登录掉线）。FIXME: 尚未测试
-                        page.once('response', res => { if (res.status() != 200) throw new Error('状态异常，重新登录') });
+                        page.once('response', res => { if (res.status() != 200) resolve(false); });
                         // 选择课程
                         await coursesFrame.waitForXPath(`//*[@value="${course}"]`).then(x => x!.click());
                         await coursesFrame.waitForXPath('//*[@id="a"]/div/div/div[2]/div[2]/input').then(x => x!.click());
                         logger.info('点击提交');
                     });
-                    await delay(1000);
+                    if (submitOk) await delay(1000);
+                    else {
+                        logger.info('状态异常，重新登录');
+                        break;
+                    }
                 }
             }
         } catch (e) {
